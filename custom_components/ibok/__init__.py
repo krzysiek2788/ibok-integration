57a497bdb91380500e4d158d7cb12ef1538e5f92
krzysiek2788 <61471407+krzysiek2788@users.noreply.github.com>
2024-03-21 10:53:06 +0100

    Update __init__.py

--- custom_components/ibok/__init__.py
+++ custom_components/ibok/__init__.py
@@ -2,7 +2,7 @@ DOMAIN = "ibok"
 
 
 async def async_setup(hass, config):
-    hass.states.async_set("hello_state.world", "Paulus")
+    hass.states.async_set("ibok.world", "Paulus")
 
     # Return boolean to indicate that initialization was successful.
     return True
