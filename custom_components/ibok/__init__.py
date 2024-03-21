ec0c32d237f14806184119e13ce1b8be3ed84cab
krzysiek2788 <61471407+krzysiek2788@users.noreply.github.com>
2024-03-21 10:57:39 +0100

    Update __init__.py

--- custom_components/ibok/__init__.py
+++ custom_components/ibok/__init__.py
@@ -1,8 +1,30 @@
+"""
+The "hello world" custom component.
+
+This component implements the bare minimum that a component should implement.
+
+Configuration:
+
+To use the hello_world component you will need to add the following to your
+configuration.yaml file.
+
+hello_world_async:
+"""
+
+from __future__ import annotations
+
+import asyncio
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
 DOMAIN = "ibok"
 
+@asyncio.coroutine
+def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Setup our skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    hass.states.async_set('ibok.Hello_World', 'Works!')
 
-async def async_setup(hass, config):
-    hass.states.async_set("ibok.world", "Paulus")
-
-    # Return boolean to indicate that initialization was successful.
+    # Return boolean to indicate that initialization was successfully.
     return True
